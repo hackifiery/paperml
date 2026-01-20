b750a21a0661db4b026621d388bc8c99512849f6
hackifiery <129475587+hackifiery@users.noreply.github.com>
2026-01-20 19:04:37 +0000

    tweak test program

--- src/test.c
+++ src/test.c
@@ -1,9 +1,16 @@
 #include "linreg.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(void) {
+    srand(time(NULL));
     Plane pl = initPlane();
-    for (int i = 0; i <= 10; i++) addPoint(&pl, initPoint(i,i));
+    for (int i = 0; i <= 10; i++) {
+        double r1 = (double)rand() / (double)RAND_MAX*1;
+        double r2 = (double)rand() / (double)RAND_MAX*1;
+        addPoint(&pl, initPoint(i + r1, i + r2));
+    }
     LinReg lr = linreg(pl);
     printLinReg(lr);
     return 0;
